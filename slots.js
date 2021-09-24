f288a4896caa98fd900ade9190fdd443962d4add
Martijn5V <112191@emmauscollege.nl>
2021-09-24 08:26:48 +0000

    bezig met slots

---
+++ slots.js
@@ -0,0 +1,25 @@
+class slots {
+    x;
+    y;
+    width;
+    height;
+    
+
+    constructor(_x, _y, _width, _height) {
+        this.x = _x;
+        this.y = _y;
+        this.width = _width;
+        this.height = _height;
+    }
+
+    show() {
+        fill(255,0,0);
+        ()
+    }
+
+
+
+
+
+
+}
